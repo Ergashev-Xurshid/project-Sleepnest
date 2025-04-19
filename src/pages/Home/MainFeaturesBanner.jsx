cf4d98a8a0b31c9a2385e793f60e9185e91e8299
Ergashev-Xurshid <xurshidgithub@gmail.com>
2025-04-19 20:13:35 +0500

    add commit

--- src/pages/Home/MainFeaturesBanner.jsx
+++ src/pages/Home/MainFeaturesBanner.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 function MainFeaturesBanner({bannerIMG , style}) {
   const {t} = useTranslation()
   return (
-    <div className={`${style} pt-[200px] mt-[200px] features-banner-shadow xl:mt-[100px] relative text-white flex flex-wrap max-md:justify-center max-md:text-center items-center justify-between rounded-[30px] p-[20px] lg:p-[70px] gap-[20px]`}>
-      <div className='md:flex-1 xl:flex-2 '>
+    <div className={`${style} pt-[200px] mt-[200px] features-banner-shadow xl:mt-[100px] relative text-white flex flex-wrap max-md:justify-center max-md:text-center items-center xl:justify-between rounded-[30px] p-[20px] lg:p-[100px] gap-[20px]`}>
+      <div className=' xl:flex-2 '>
         <p className='font-[800] text-[50px]  xl:text-[90px] mb-5'>100%</p>
         <p className='text-[20px]'>{t("hero-baner-text")}</p>
       </div>
-      <div className='w-full md:w-[800px]  h-auto absolute ] left-0 md:left-[20] top-[-280px] md:top-[-320px]  xl:left-[23%] '>
+      <div className='w-full xl:w-[800px]  h-auto absolute left-0 md:left-[20] top-[-100px] md:top-[-425px]  xl:top-[-80px]  xl:left-[20%] '>
         <img className=' h-auto w-[100%]' src={bannerIMG} alt="Banner" />
       </div>
       <div className='w-full md:flex-1 z-20 '>
